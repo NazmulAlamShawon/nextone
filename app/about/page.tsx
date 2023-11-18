9869878746365477b557bee6fe83023b75a38c06
NazmulAlamShawon <shawonkst15@gmail.com>
2023-11-18 17:04:20 +0600

    add another new link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
 export default function page () {
     return (
         <div>
             <h1>hello about</h1>
+            <Link href="/">
+               Link to hpme page
+            </Link>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 
 
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main >
       <h1>hello</h1>
+      <Link href="/about">
+        go to about page
+      </Link>
     </main>
   )
 }

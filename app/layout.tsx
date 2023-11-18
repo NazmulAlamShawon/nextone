0ebf19ff9b9492235f478507d5fa4bde3204e7c4
NazmulAlamShawon <shawonkst15@gmail.com>
2023-11-18 16:57:25 +0600

    add about new layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,11 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+         <nav> <h1>my nav</h1></nav>
+        {children}
+        
+        </body>
     </html>
   )
 }
